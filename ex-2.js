852d985164c01ee6f1f2de55cfdad860b03af3ab
Kunakorn <kunakorn.ph98@gmail.com>
2024-05-17 15:11:36 +0700

    Assignment#1 Call back

--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,38 @@
 //Exercise #2: At Least Five Function
 
 function atLeastFive(array, operation) {
-  // Start coding here
+  if(operation(array) >= 5){
+    return "ผ่านเกณฑ์ ✅"
+  }
+  else{
+    return "ไม่ผ่านเกณฑ์ ❌"
+  }
+
+}
+function check(result){
+  console.log(result.length)
+  let checkTrue = []
+  for (let i = 0 ; i < result.length; i++){
+    if(result[i] > 70){
+      checkTrue.push(result[i])
+    }
+  }
+  console.log(checkTrue.length)
+  return checkTrue.length
+
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
+
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1,check);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2,check);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3,check);
+
+console.log("นักเรียนห้องที่ 1 " + scoreRoom1Result)
+console.log("นักเรียนห้องที่ 2 " + scoreRoom2Result)
+console.log("นักเรียนห้องที่ 3 " + scoreRoom3Result)
